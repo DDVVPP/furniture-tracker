ec62dbe93e7ed9a687cfa8ba6dae9a21f5604562
Darshin <darshinvp@gmail.com>
2023-10-01 13:53:07 -0700

    update: Form component

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,11 +1,40 @@
 import Link from "next/link";
 
-const Form = ({ project, room, handleSubmit, setItem, item }: any) => {
+const Form = ({
+  project,
+  room,
+  handleSubmit,
+  setItem,
+  item
+}: any): JSX.Element => {
+
+  const formInput = (
+    label: string,
+    input: string,
+    placeholder: string
+    ): JSX.Element => {
+    return (
+      <label>
+        <span className="font-satoshi font-semibold text-sm text-gray-700">
+          {label} {` `}
+        </span>
+
+        <input
+          value={input}
+          onChange={(e) => setItem({ ...item, input: e.target.value })}
+          placeholder={placeholder}
+          required
+          className="form_input"
+        />
+      </label>
+    )
+  }
+
   return (
     <section className="w-full max-w-full flex-start flex-col">
       <h1 className="subhead_text text-left">
         <span className="blue_gradient">
-          {project.name}
+          {project && project.name}
         </span>
       </h1>
       <h2 className="mt-2 text-left font-semibold">
@@ -14,50 +43,25 @@ const Form = ({ project, room, handleSubmit, setItem, item }: any) => {
         </span>
       </h2>
 
-      <form onSubmit={handleSubmit}
+      <form
+        onSubmit={handleSubmit}
         className="mt-5 p-8 w-full max-w-2xl flex flex-col gap-4 rounded-xl border border-gray-200 bg-gray-100">
-        <label>
-          <span className="font-satoshi font-semibold text-sm text-gray-700">
-            *Supplier {` `}
-          </span>
 
-          <input
-            value={item.supplier}
-            onChange={(e) => setItem({...item, supplier: e.target.value})}
-            placeholder="Knoll"
-            required
-            className="form_input"
-            />
-        </label>
-
-
-        <label>
-          <span className="font-satoshi font-semibold text-sm text-gray-700">
-            Item {` `}
-          </span>
-
-          <input
-            value={item.item}
-            onChange={(e) => setItem({ ...item, item: e.target.value })}
-            placeholder="Marcel Breuer Chaise Lounge"
-            required
-            className="form_input"
-          />
-        </label>
-
-        <label>
-          <span className="font-satoshi font-semibold text-sm text-gray-700">
-            Finish {` `}
-          </span>
-
-          <input
-            value={item.finish}
-            onChange={(e) => setItem({ ...item, finish: e.target.value })}
-            placeholder="Natural oak frame with saddle H cognac leather"
-            required
-            className="form_input"
-          />
-        </label>
+        {
+          formInput(
+            'Supplier', item.supplier, 'Knoll'
+          )
+        }
+        {
+          formInput(
+            'Item', item.item, "Marcel Breuer Chaise Lounge"
+          )
+        }
+        {
+          formInput(
+            'Finish', item.finish, "Natural oak frame with saddle H cognac leather"
+          )
+        }
 
         <div className="flex-end mt-4 gap-4">
           <Link href='/' className="mr-3 text-gray-500 text-sm">
@@ -72,6 +76,7 @@ const Form = ({ project, room, handleSubmit, setItem, item }: any) => {
             {false ? 'Saving...' : 'Save'}
           </button>
         </div>
+
       </form>
     </section>
   )
