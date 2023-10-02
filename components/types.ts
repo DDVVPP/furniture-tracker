9fa42427c71e0d79e70e4fe98b0e8bf9f78fa1b8
Darshin <darshinvp@gmail.com>
2023-10-02 13:13:49 -0700

    fix: typos

--- components/types.ts
+++ components/types.ts
@@ -1,29 +1,29 @@
 import { Dispatch, FormEventHandler, SetStateAction } from 'react';
 
 export type ProjectType = {
-  id: string,
-  name: string,
-  image: string,
+  id: string;
+  name: string;
+  image: string;
 }
 
 export type ItemType = {
-  name: string,
-  supplier: string,
-  finish: string,
+  name: string;
+  supplier: string;
+  finish: string;
 }
 
 export type ItemFormProps = {
-  project: ProjectType,
-  room: string,
-  setItem: Dispatch<SetStateAction<ItemType>>,
-  item: ItemType,
-  isSaving: boolean,
-  handleSubmit: FormEventHandler<HTMLFormElement>,
+  project: ProjectType;
+  room: string;
+  setItem: Dispatch<SetStateAction<ItemType>>;
+  item: ItemType;
+  isSaving: boolean;
+  handleSubmit: FormEventHandler<HTMLFormElement>;
 }
 
 export type TextFieldType = {
-  label: string,
-  textInput: string,
-  placeholder: string,
-  isRequired: boolean,
+  label: string;
+  textInput: string;
+  placeholder: string;
+  isRequired: boolean;
 }
